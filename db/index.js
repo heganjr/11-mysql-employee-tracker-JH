0d44c9d7be48ef4cfa452bd324b3eab2cf179fbf
heganjr <jacobhegan@hotmail.com>
2021-05-24 14:01:58 +0800

    npm packages added sql2 inquirer connection to sql added

--- db/index.js
+++ db/index.js
@@ -1,3 +1,13 @@
+const mysql = require("mysql2");
+const inquirer = require(`inquirer`)
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Eagles!168",
+  database: "greatBayDB",
+});
 
 // create database based on readme instructions - DONE
 
