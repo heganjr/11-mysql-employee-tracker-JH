830cbf87a031cb167a681274571c28ab7222fbec
heganjr <jacobhegan@hotmail.com>
2021-05-24 19:50:21 +0800

    added add employee prompts

--- db/index.js
+++ db/index.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2");
-const inquirer = require(`inquirer`)
+const inquirer = require(`inquirer`);
+// linking modules
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -9,6 +10,90 @@ const connection = mysql.createConnection({
   database: "employeeDB",
 });
 
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log(`connected to ${connection.config.database}`);
+// });
+// Confuses prompt with an additional line
+
+async function initialise() {
+  const answers = await inquirer.prompt([
+    {
+      type: "list",
+      name: "task",
+      choices: ["Add", "View", "Update"],
+      message: "What would you like to do?",
+    },
+  ]);
+  console.log(answers.task);
+
+  switch (answers.task) {
+    case "Add":
+      addIntoEmployeeDB();
+      console.log("Add");
+      break;
+
+    case "View":
+      viewEmployeeDB();
+      console.log("View");
+      break;
+
+    case "Update":
+      updateEmployeeDB();
+      console.log("Update");
+      break;
+  }
+}
+
+async function addIntoEmployeeDB() {
+  const tableAnswer = await inquirer.prompt([
+    {
+      type: "list",
+      name: "chosenTable",
+      message: "Where would you like to add?",
+      choices: ["Employees", "Departments", "Roles"],
+    },
+  ]);
+
+  switch (tableAnswer.chosenTable) {
+    case "Employees":
+      employeeAdd();
+      break;
+  }
+}
+
+async function employeeAdd() {
+  const employeeAnswers = await inquirer.prompt(
+  [
+    {
+      type: "input",
+      name: "first_name",
+      message: "Enter the employee's first name",
+    },
+    {
+      type: "input",
+      name: "last_name",
+      message: "Enter the employee's last name",
+    },
+    {
+      type: "input",
+      name: "role",
+      message: "What is the employee's role?",
+      // provide a list from exisiting roles?
+    },
+    {
+      type: "input",
+      name: "manager",
+      message: "Who is the employee's manager?",
+      // provide a list from existing managers?
+    },
+  ]);
+  console.log(employeeAnswers)
+  connection.end()
+}
+
+initialise();
+
 // create database based on readme instructions - DONE
 
 //Build a command-line application that users can:
@@ -16,7 +101,7 @@ const connection = mysql.createConnection({
 // Add departments, roles, employees tables
 
 //employee table:
-//prompt for employee first name, last name, manager name, role title
+//prompt for employee first name, last name, manager name, role title - Done
 //based on manager name, get manager id
 //based on role title, get role id
 //isert record into database
